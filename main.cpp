3c60dc2fbed3d972697cdbe1afd573af00e661f3
thomas052001 <bread052001@gmail.com>
2026-05-27 07:55:02 +0000

    homework

--- main.cpp
+++ main.cpp
@@ -44,17 +44,17 @@ void printMatrix(const vector<vector<int>>& D) {
 vector<vector<int>> floydWarshall(vector<vector<int>> D) {
     int n = D.size();
 
-    // TODO:
-    // 使用三層迴圈完成 Floyd-Warshall
-    //
-    // 提示：
-    // for k = 0 到 n-1
-    //   for i = 0 到 n-1
-    //     for j = 0 到 n-1
-    //       若 D[i][k] 和 D[k][j] 都不是 INF
-    //       且 D[i][k] + D[k][j] < D[i][j]
-    //          更新 D[i][j]
-
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (D[i][k] != INF && D[k][j] != INF) {
+                    if (D[i][k] + D[k][j] < D[i][j]) {
+                        D[i][j] = D[i][k] + D[k][j];
+                    }
+                }
+            }
+        }
+    }
     return D;
 }
 
@@ -67,20 +67,32 @@ int main() {
     // 初始化距離矩陣 W
     vector<vector<int>> W(n, vector<int>(n, INF));
 
-    // TODO:
     // 1. 將對角線 W[i][i] 設為 0
-    //
-    // 2. 依照題目中的圖加入邊：
+    for (int i = 0; i < n; i++) {
+        W[i][i] = 0;
+    }
+    
+    // 2. 依照題目中的圖加入邊 (注意：陣列是 0-based，節點是 1-based，所以要減 1)
     // 1 -> 2 = 3
+    W[0][1] = 3;
     // 1 -> 3 = 5
+    W[0][2] = 5;
     // 3 -> 2 = -2
+    W[2][1] = -2;
     // 2 -> 5 = 7
+    W[1][4] = 7;
     // 3 -> 4 = -5
+    W[2][3] = -5;
     // 3 -> 5 = -5
+    W[2][4] = -5;
     // 2 -> 4 = 2
+    W[1][3] = 2;
     // 4 -> 5 = 6
+    W[3][4] = 6;
     // 4 -> 6 = 10
+    W[3][5] = 10;
     // 5 -> 6 = 8
+    W[4][5] = 8;
 
     cout << "Initial distance matrix W:\n";
     printMatrix(W);
